888dad3ac7488dba0e831288fde8e75cd6230826
uj-on-kaist <jung-1028@kaist.ac.kr>
2015-01-27 23:06:32 +0900

    Initial Commit

---
+++ Olive-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Olive-iOS
+//
+//  Created by 정의준 on 2015. 1. 27..
+//  Copyright (c) 2015년 정의준. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
